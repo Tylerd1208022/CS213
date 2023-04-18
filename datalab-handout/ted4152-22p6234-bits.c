56a66117c71e8898113207337dc5fd44f704a87a
Tyler Ethan Dempski <ted4152@joker.ece.northwestern.edu>
2023-04-18 14:41:23 -0500

    Lab 2 setup

---
+++ datalab-handout/ted4152-22p6234-bits.c
@@ -0,0 +1,409 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * <Please put your name and userid here>
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#if 0
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+INTEGER CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+  7. Use any data type other than int.  This implies that you
+     cannot use arrays, structs, or unions.
+
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting if the shift amount
+     is less than 0 or greater than 31.
+
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+FLOATING POINT CODING RULES
+
+For the problems that require you to implement floating-point operations,
+the coding rules are less strict.  You are allowed to use looping and
+conditional control.  You are allowed to use both ints and unsigneds.
+You can use arbitrary integer and unsigned constants. You can use any arithmetic,
+logical, or comparison operations on int or unsigned data.
+
+You are expressly forbidden to:
+  1. Define or use any macros.
+  2. Define any additional functions in this file.
+  3. Call any functions.
+  4. Use any form of casting.
+  5. Use any data type other than int or unsigned.  This means that you
+     cannot use arrays, structs, or unions.
+  6. Use any floating point data types, operations, or constants.
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operations (integer, logical,
+     or comparison) that you are allowed to use for your implementation
+     of the function.  The max operator count is checked by dlc.
+     Note that assignment ('=') is not counted; you may use as many of
+     these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. Use the BDD checker to formally verify your functions
+  5. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+
+/*
+ * STEP 2: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the BDD checker to formally verify that your solutions produce 
+ *      the correct answers.
+ */
+
+
+#endif
+/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+/* This header is separate from features.h so that the compiler can
+   include it implicitly at the start of every compilation.  It must
+   not itself include <features.h> or any other header that includes
+   <features.h> because the implicit include comes before any feature
+   test macros that may be defined in a source file before it first
+   explicitly includes a system header.  GCC knows the name of this
+   header in order to preinclude it.  */
+/* glibc's intent is to support the IEC 559 math functionality, real
+   and complex.  If the GCC (4.9 and later) predefined macros
+   specifying compiler intent are available, use them to determine
+   whether the overall intent is to support these features; otherwise,
+   presume an older compiler has intent to support these features and
+   define these macros by default.  */
+/* wchar_t uses Unicode 10.0.0.  Version 10.0 of the Unicode Standard is
+   synchronized with ISO/IEC 10646:2017, fifth edition, plus
+   the following additions from Amendment 1 to the fifth edition:
+   - 56 emoji characters
+   - 285 hentaigana
+   - 3 additional Zanabazar Square characters */
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitXor(int x, int y) {
+  //op1 and op2 comprise of all bits which xor is true,
+  //then demorgans law is used to provide a functional equivalent
+  //of OR to produce a combined result
+  int op1 = x&(~y);//2
+  int op2 = y&(~x);//4
+  op1 = ~op1;//5
+  op2 = ~op2;//6
+  return ~(op1&op2);//7 demorgan
+}
+/* 
+ * bitMatch - Create mask indicating which bits in x match those in y
+ *            using only ~ and & 
+ *   Example: bitMatch(0x7, 0xfffffffE) = 0x6
+ *   Legal ops: ~ & |
+ *   Max ops: 14
+ *   Rating: 1
+ */
+int bitMatch(int x, int y) {
+  //matchOnes/Zeroes find bits that match
+  //then apply demorgans to combine with functional equivalent of OR
+  int matchOnes = x & y;
+  int matchZeroes = ~x & ~y;
+  return ~(~matchZeroes&~matchOnes);
+}
+/* 
+ * allOddBits - return 1 if all odd-numbered bits in word set to 1
+ *   where bits are numbered from 0 (least significant) to 31 (most significant)
+ *   Examples allOddBits(0xFFFFFFFD) = 0, allOddBits(0xAAAAAAAA) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 2
+ */
+int allOddBits(int x) {
+  //Use oddOnes as mask, compare with oddOnes using xor
+  //If any dont match, return false, else true
+  int oddOnes = 0xaa;
+  oddOnes = (oddOnes << 8) | 0xaa;
+  oddOnes = (oddOnes << 8) | 0xaa;
+  oddOnes = (oddOnes << 8) | 0xaa;
+  //return oddOnes;
+  return !((x & oddOnes) ^ oddOnes);
+}
+/* 
+ * byteSwap - swaps the nth byte and the mth byte
+ *  Examples: byteSwap(0x12345678, 1, 3) = 0x56341278
+ *            byteSwap(0xDEADBEEF, 0, 2) = 0xDEEFBEAD
+ *  You may assume that 0 <= n <= 3, 0 <= m <= 3
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 25
+ *  Rating: 2
+ */
+int byteSwap(int x, int n, int m) {
+  //Finds the two bytes, seperates them, clears the two byte slots in x,
+  //combines bytes to be swapped and cleared x
+    int mshamt = m <<3;
+    int nshamt = n << 3;
+    int byteOneMask = 0x000000FF << nshamt;
+    int byteTwoMask = 0x000000FF << mshamt;
+    int bytesMask = byteOneMask | byteTwoMask;
+    int byteOne = x & byteOneMask; 
+    int byteTwo = x & byteTwoMask; 
+    int clearedX = x & ~bytesMask;
+    int byteOneInPosition = ((byteOne >> nshamt) << mshamt) & byteTwoMask;
+    int byteTwoInPosition = ((byteTwo >> mshamt) << nshamt) & byteOneMask;
+    return byteOneInPosition | byteTwoInPosition | clearedX;
+}
+/* 
+ * isGreater - if x > y  then return 1, else return 0 
+ *   Example: isGreater(4,5) = 0, isGreater(5,4) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isGreater(int x, int y) {
+  //Convert Y to two's complement negative
+  //subtract
+  //if positive x > y
+  //but if overflow possible (i.e. opposing signs)
+  //just check which if x is negative
+  int negativeYVal = ~y + 1;
+  int sum = negativeYVal + x;
+  int sumRes = !(sum >> 31);//1 if x > y
+  int diffSigns = ((x >> 31) ^ (y >> 31)) & 0x00000001;
+  int overflowResult = diffSigns & (y >> 31); //if y < 0 return true
+  return ((overflowResult | (sumRes & !diffSigns)) & !(!(sum)));
+}
+/* 
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
+ *            isAsciiDigit(0x05) = 0.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 3
+ */
+int isAsciiDigit(int x) {
+  int twosCompX = ~x + 1;
+  int twosCompTop = ~0x3A + 1;
+  int aboveBottomRange = ((0x2F + twosCompX) >>31 ) & 1; //1 if above range
+  int belowTopRange = ((x + twosCompTop) >> 31) & 1;
+  return aboveBottomRange & belowTopRange;
+}
+/*
+ * bitParity - returns 1 if x contains an odd number of 0's
+ *   Examples: bitParity(5) = 0, bitParity(7) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int bitParity(int x) {
+  //Perform xor reduction
+  int toSixteen = (x >> 16) ^ x;//2
+  int toEight = (toSixteen >> 8) ^ toSixteen;//4
+  int toFour = (toEight >> 4) ^ toEight;//6
+  int toTwo = (toFour >> 2) ^ toFour;//8
+  int toOne = (toTwo >> 1) ^ toTwo;
+  return toOne & 1;
+}
+/*
+ * isTmin - returns 1 if x is the minimum, two's complement number,
+ *     and 0 otherwise 
+ *   Legal ops: ! ~ & ^ | +
+ *   Max ops: 10
+ *   Rating: 1
+ */
+int isTmin(int x) {
+  return !(((~x + 1) ^ x) | !x);
+  
+}
+/* 
+ * fitsBits - return 1 if x can be represented as an 
+ *  n-bit, two's complement integer.
+ *   1 <= n <= 32
+ *   Examples: fitsBits(5,3) = 0, fitsBits(-4,3) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 2
+ */
+int fitsBits(int x, int n) {
+  int negOne = ~0;
+  int shiftedRes = (x >> (n + negOne));
+  int noRemainder = !shiftedRes | !(~shiftedRes);
+  int signMatch = !((shiftedRes & 1) ^ ((x >> 31) & 1));
+  return signMatch & noRemainder;
+}
+/* 
+ * twosComp2SignMag - Convert from two's complement to sign-magnitude 
+ *   where the MSB is the sign bit
+ *   You can assume that x > TMin
+ *   Example: twosComp2SignMag(-5) = 0x80000005.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 4
+ */
+int twosComp2SignMag(int x) {
+  //If positive, stays the same
+  //else, invert sign, or with 0x80000000
+  int longerConstant = 0x80 << 24;
+  int inverted = ~x + 1;
+  int isPositive = (!(x & longerConstant)) << 31;
+  int negOutput = inverted | longerConstant;
+  return ((isPositive >> 31) & x) | (~(isPositive >> 31) & negOutput);
+}
+/* 
+ * floatIsEqual - Compute f == g for floating point arguments f and g.
+ *   Both the arguments are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   If either argument is NaN, return 0.
+ *   +0 and -0 are considered equal.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 25
+ *   Rating: 2
+ */
+int floatIsEqual(unsigned uf, unsigned ug) {
+    if (!(~(uf | 0x803FFFFF))) return 0; //if either nan
+    if (!(~(ug | 0x803FFFFF))) return 0;
+    if (!((uf & 0x7FFFFFFF)||(ug & 0x7FFFFFFF))) return 1; //if both either form of 0
+    return !(uf ^ ug);
+}
+/* 
+ * floatNegate - Return bit-level equivalent of expression -f for
+ *   floating point argument f.
+ *   Both the argument and result are passed as unsigned int's, but
+ *   they are to be interpreted as the bit-level representations of
+ *   single-precision floating point values.
+ *   When argument is NaN, return argument.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 10
+ *   Rating: 2
+ */
+unsigned floatNegate(unsigned uf) {
+  if (!(~(uf | 0x803FFFFF))) return uf;//if  NAN (all exponent 1)
+  if (uf & 0x80000000) return (uf & 0x7FFFFFFF);//If negative make sign 0
+  return (uf | 0x80000000);//If positive make sign 1
+}
+/* 
+ * floatFloat2Int - Return bit-level equivalent of expression (int) f
+ *   for floating point argument f.
+ *   Argument is passed as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point value.
+ *   Anything out of range (including NaN and infinity) should return
+ *   0x80000000u.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ */
+int floatFloat2Int(unsigned uf) {
+  int expDigits = (uf & 0x7F800000);
+  int expVal = -127 + (expDigits >> 23);
+  int accum = 1;
+  int frac = (uf & 0x007FFFFF) | 0x00800000; //add implied one
+  int maxFull = 0;
+  if (!(~(uf | 0x803FFFFF))) return 0x80000000;//if nan or inf return
+  if (!(uf & 0x7FFFFFFF)) return 0;
+  if ((expVal & 0x80000000)||!expDigits) return 0x00000000;//if negative exponent or denorm, return 0
+  //expVal = expVal + 1;
+  while(expVal){
+    if (frac & 0x00800000) accum = accum + 1;//put correct value from curr frac into accum 32-expVal'th bit
+    frac = frac << 1;//reduce exp, look at next bit for shifting
+    expVal = expVal + -1;//Reduce remaining expval
+    maxFull = accum & 0x80000000;
+    accum = accum << 1;
+    if(expVal && maxFull) return 0x80000000;//if overflow (loop will continue and last bit occupied) return
+  }
+  if (uf & 0x80000000) return ~accum + 1;//24
+  return accum;
+  return 2;
+}
